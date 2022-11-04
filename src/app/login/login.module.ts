1e7ba043c55c2ef4fe9ae15b48d70cd2547d387e
ice-wind <romanwarga@gmail.com>
2022-11-05 00:08:43 +0100

    init commit

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { LogFormComponent } from './log-form/log-form.component';
+
+
+@NgModule({
+  declarations: [LogFormComponent],
+  imports: [
+    
+  ]
+})
+export class LoginModule { }
